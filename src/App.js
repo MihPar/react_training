85c6552ac544751b0e6cff0255e4ae3a84387193
MihPar <9496260@gmail.com>
2023-07-28 21:18:09 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
+import object from "./App.module.css"
+import { useState } from "react";
+
 export default function Board() {
+    let [squares, setSquares] = useState(Array(9).fill(null))
+
+    function handleClick() {
+        let nextSquares = squares.slice()
+        nextSquares[0] = "X"
+        setSquares(nextSquares)
+    }
+
     return (
       <div>
-        <div className="board-row">
-          <Square value="1" />
-          <Square value="2" />
-          <Square value="3" />
+        <div>
+          <Square value={squares[0]} onSquareClick={handleClick} />
+          <Square value={squares[1]} />
+          <Square value={squares[2]} />
         </div>
-        <div className="board-row">
-          <Square value="4" />
-          <Square value="5" />
-          <Square value="6" />
+        <div>
+          <Square value={squares[3]} />
+          <Square value={squares[4]} />
+          <Square value={squares[5]} />
         </div>
-        <div className="board-row">
-          <Square value="7" />
-          <Square value="8" />
-          <Square value="9" />
+        <div>
+          <Square value={squares[6]} />
+          <Square value={squares[8]} />
+          <Square value={squares[9]} />
         </div>
       </div>
     );
   }
   
-  function Square(props) {
-    return <button className="square">{props.value}</button>;
+  function Square({value, onSquareClick}) {
+    return <button className={object.square} onClick={onSquareClick}>{value}</button>;
   }
   
--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Board from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Board />)
 
-function Clock(props) {
-    return (
-      <div>
-        <h1>Привет, мир!</h1>
-        <h2>Сейчас {props.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-  
-  function tick() {
-    root.render(<Clock date={new Date()} />);
-  }
-  
-  setInterval(tick, 1000);
-
-reportWebVitals();
