f93a68dc2111d52fb1793a737c6edddfcf283667
MihPar <9496260@gmail.com>
2023-07-31 20:36:49 +0300

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
 import object from "./App.module.css"
 import { useState } from "react";
 
-export default function Board() {
+function Board() {
+    function handleClick(i) {
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+          }
+          const nextSquares = squares.slice();
+          if (xIsNext) {
+            nextSquares[i] = "X";
+          } else {
+            nextSquares[i] = "O";
+          }
+          onPlay(nextSquares);
+    }
+    let [xIsNext, setXIsNext] = useState(true)
     let [squares, setSquares] = useState(Array(9).fill(null))
 
-    function handleClick() {
+    function handleClick(i) {
+        if(squares[i] || calculateWinner(squares)) {
+            return 
+        }
         let nextSquares = squares.slice()
-        nextSquares[0] = "X"
+        if(xIsNext) {
+             nextSquares[i] = "X"
+        } else {
+             nextSquares[i] = "O"
+        }
         setSquares(nextSquares)
+        setXIsNext(!xIsNext)
+    }
+
+    let winner = calculateWinner(squares)
+    let status
+    if (winner) {
+        status = "Winner" + winner
+    } else {
+        status = "Next player" + (xIsNext ? "X" : "O")
     }
 
     return (
       <div>
+        <div className={status}>{status}</div>
         <div>
-          <Square value={squares[0]} onSquareClick={handleClick} />
-          <Square value={squares[1]} />
-          <Square value={squares[2]} />
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
         </div>
         <div>
-          <Square value={squares[3]} />
-          <Square value={squares[4]} />
-          <Square value={squares[5]} />
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
         </div>
         <div>
-          <Square value={squares[6]} />
-          <Square value={squares[8]} />
-          <Square value={squares[9]} />
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
         </div>
       </div>
     );
@@ -34,4 +64,46 @@ export default function Board() {
   function Square({value, onSquareClick}) {
     return <button className={object.square} onClick={onSquareClick}>{value}</button>;
   }
-  
+
+  export default function Game({xIsNext, squares, onPlay}) {
+    
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquares = history[history.length - 1]
+    
+    function handlePlay(nextSquares) {
+        setHistory([...history, nextSquares])
+        setXIsNext(!xIsNext)
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board  xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        </div>
+        <div className="game-info">
+          <ol>{/*TODO*/}</ol>
+        </div>
+      </div>
+    );
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import Board from "./App";
+import Game from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Board />)
+root.render(<Game />)
 
